d60c9bbc7025f5eaad945e5191ddc9c280734f8d
Fede PC <fede.marquesto@gmail.com>
2025-06-06 19:02:43 -0300

    url query builder

---
+++ tests/QueryBuilderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use fmarquesto\SapBusinessOneConnector\QueryBuilder;
+use PHPUnit\Framework\TestCase;
+
+class QueryBuilderTest extends TestCase
+{
+    public function test_query_builder_build_url_with_select_and_filter(): void
+    {
+        $queryBuilder = new QueryBuilder('Items', 'ItemCode');
+        $queryBuilder->addSelect('ItemCode', 'ItemName')->addFilter('ItemCode eq \'123\'');
+
+        $url = $queryBuilder->buildUrl();
+        $this->assertEquals(
+            rawurlencode('Items(ItemCode)?$select=ItemCode,ItemName&$filter=ItemCode eq \'123\''),
+            $url
+        );
+    }
+
+    public function test_query_builder_builds_url_with_key_without_query_params(): void
+    {
+        $queryBuilder = new QueryBuilder('Items', 'ItemCode');
+
+        $url = $queryBuilder->buildUrl();
+
+        $this->assertEquals(rawurlencode('Items(ItemCode)'), $url);
+    }
+
+    public function test_query_builder_builds_url_without_query_params(): void
+    {
+        $queryBuilder = new QueryBuilder('Items');
+
+        $url = $queryBuilder->buildUrl();
+
+        $this->assertEquals(rawurlencode('Items'), $url);
+    }
+
+}
